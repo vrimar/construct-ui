0d38db296cd6f0f92c61f84c40caa5bed116386c
Vasil Rimar <5660247+vrimar@users.noreply.github.com>
2019-03-10 15:14:22 -0500

    Textarea: add tests

---
+++ src/components/text-area/index.spec.ts
@@ -0,0 +1,47 @@
+import m from 'mithril';
+import assert from 'assert';
+import { TextArea, ITextAreaAttrs, Classes } from '@/';
+import { hasClass } from '@test-utils';
+
+describe.only('textarea', () => {
+  const el = () => document.body.firstChild as HTMLElement;
+  const textarea = () => el().querySelector('textarea');
+
+  afterEach(() => m.mount(document.body, null));
+
+  it('Renders correctly', () => {
+    mount({
+      class: Classes.POSITIVE,
+      style: 'color:red',
+      intent: 'primary',
+      size: 'xs',
+      fluid: true
+    });
+
+    assert(hasClass(el(), Classes.INPUT));
+    assert(hasClass(el(), Classes.POSITIVE));
+    assert(hasClass(el(), Classes.PRIMARY));
+    assert(hasClass(el(), Classes.XS));
+    assert(hasClass(el(), Classes.FLUID));
+    assert(el().hasAttribute('style'));
+  });
+
+  it('Passes through html attrs', () => {
+    mount({
+      id: 1,
+      name: 'name',
+      defaultValue: 'defaultValue'
+    });
+
+    assert(textarea().hasAttribute('id'));
+    assert(textarea().hasAttribute('name'));
+    assert.equal(textarea().value, 'defaultValue');
+  });
+
+  function mount(attrs: ITextAreaAttrs) {
+    const component = {
+      view: () => m(TextArea, { ...attrs })
+    };
+    m.mount(document.body, component);
+  }
+});
